0fa17250dde5cdb1dd21171c8ee2fdc26a43bb3c
gavinlaucn <logicbiz@qq.com>
2016-01-19 16:05:54 +0800

    init portlet

---
+++ docroot/WEB-INF/src/com/j2eecn/mcat/MeCategoryPortlet.java
@@ -0,0 +1,11 @@
+package com.j2eecn.mcat;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class MeCategoryPortlet
+ */
+public class MeCategoryPortlet extends MVCPortlet {
+ 
+
+}
